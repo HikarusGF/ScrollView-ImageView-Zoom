451f5bc595ce157f00dc26e4fb5f37ea7855e867
Hikárus Guijarro-Fayard <root@hikarian.space>
2020-01-08 19:57:20 +0100

    initial Commit

--- ScrollViewZoom/ViewController.swift
+++ ScrollViewZoom/ViewController.swift
@@ -2,19 +2,37 @@
 //  ViewController.swift
 //  ScrollViewZoom
 //
-//  Created by Hikarus on 08/01/2020.
-//  Copyright © 2020 Hikarian. All rights reserved.
+//  Created by Hikarus Guijarro-Fayard on 1/8/20.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+	@IBOutlet var ⓞscrollView: UIScrollView!
+	
+	let imageView🅒: UIImageView = {
+		let outputIV🅒 = UIImageView()
+		outputIV🅒.translatesAutoresizingMaskIntoConstraints = false
+      outputIV🅒.image = UIImage(named: "BDSM.jpg")
+      outputIV🅒.contentMode = .scaleAspectFill
+      return outputIV🅒
+   }()
+   
    override func viewDidLoad() {
       super.viewDidLoad()
-      // Do any additional setup after loading the view.
-   }
-
-
+      ⓞscrollView.addSubview(imageView🅒)
+      NSLayoutConstraint.activate([
+         imageView🅒.topAnchor.constraint(equalTo: ⓞscrollView.contentLayoutGuide.topAnchor, constant: 0.0),
+         imageView🅒.bottomAnchor.constraint(equalTo: ⓞscrollView.contentLayoutGuide.bottomAnchor, constant: 0.0),
+         imageView🅒.leadingAnchor.constraint(equalTo: ⓞscrollView.contentLayoutGuide.leadingAnchor, constant: 0.0),
+         imageView🅒.trailingAnchor.constraint(equalTo: ⓞscrollView.contentLayoutGuide.trailingAnchor, constant: 0.0),
+      ])
+      ⓞscrollView.minimumZoomScale = 0.2
+      ⓞscrollView.maximumZoomScale = 5.0
+	}
+	
+	func viewForZooming(in scrollView: UIScrollView) -> UIView? { return imageView🅒 }
+	
 }
 
